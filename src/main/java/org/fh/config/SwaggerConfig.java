585d40ac0515bc87adde874ad658aef1587eda2f
Kazusa Touma <maolinwang1998@gmail.com>
2021-05-08 16:57:38 +0800

    add v1.0 source code

---
+++ src/main/java/org/fh/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package org.fh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 说明：Swagger 接口API生成
+ * 作者：FH Admin Q313596790
+ * 官网：www.fhadmin.org
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("org.fh.controller"))	// 为当前包路径
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("FH Admin Swagger2 RESTful API") 	// 页面标题
+				.version("1.0")								// 版本号
+				.description("www.fhadmin.org").build();	// 描述
+	}
+
+}
